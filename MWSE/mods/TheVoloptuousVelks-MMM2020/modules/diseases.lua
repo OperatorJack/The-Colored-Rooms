0939a9a6644bdb0fe526081b99e9d521544694f6
OperatorJack <thepaladinof@gmail.com>
2020-12-04 18:47:52 -0500

    Update diseases.lua

--- MWSE/mods/TheVoloptuousVelks-MMM2020/modules/diseases.lua
+++ MWSE/mods/TheVoloptuousVelks-MMM2020/modules/diseases.lua
@@ -4,13 +4,9 @@ local function getRandomDisease()
     return tes3.getObject(common.diseases[math.random(#common.diseases)].id)
 end
 
-
-
 event.register("TheColoredRooms:TriggerDisease", function(e)
-    local disease = getRandomDisease()
-    
     mwscript.addSpell({
         reference = e.reference, 
-        spell = disease
+        spell = getRandomDisease()
     })
 end)
