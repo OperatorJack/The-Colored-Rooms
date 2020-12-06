a5595e68b41f0000d50c6469eba0966a3f42b142
Greatness7 <Greatness7@gmail.com>
2020-12-06 11:42:53 -0500

    reorganize traps.lua to avoid forward declarations (no logic changes)

--- MWSE/mods/TheVoloptuousVelks-MMM2020/modules/traps.lua
+++ MWSE/mods/TheVoloptuousVelks-MMM2020/modules/traps.lua
@@ -3,24 +3,22 @@ local common = require("TheVoloptuousVelks-MMM2020.common")
 local function isTrapTriggered(trap)
     return trap.data.VV20 ~= nil and trap.data.VV20.triggered == true
 end
+
 local function setTrapTriggered(trap)
     trap.data.VV20 = trap.data.VV20 or {}
     trap.data.VV20.triggered = true
 end
 
+
 -- Trap Controller for non-event based traps.
 local activeTimerTraps = {}
-local timerTrapCallback
-
 local activeProximityTraps = {}
-local proximityTrapCallback
 
 local function onReferenceActivated(e)
     if common.traps.timer[e.reference.object.id] then
         activeTimerTraps[e.reference] = tes3.getSimulationTimestamp() + math.random(5)
         common.debug("Activated Timer trap: %s", e.reference)
     end
-
     if common.traps.proximity[e.reference.object.id] then
         activeProximityTraps[e.reference] = true
         common.debug("Activated Proximity trap: %s", e.reference)
@@ -34,23 +32,10 @@ local function onReferenceDeactivated(e)
 end
 event.register("referenceDeactivated", onReferenceDeactivated)
 
-local function trapTimerCallback()
-    for trapReference in pairs(activeTimerTraps) do
-        timerTrapCallback(trapReference)
-    end
-    for trapReference in pairs(activeProximityTraps) do
-        proximityTrapCallback(trapReference)
-    end
-end
-
-event.register("loaded", function()
-    timer.start{iterations = -1, duration = 0.15, callback=trapTimerCallback}
-end)
-
 
 -- Timer Traps
 -- TODO: Add delta time check to change how often they trigger.
-timerTrapCallback = function(trap)
+local function timerTrapCallback(trap)
     local nextProcessTime = activeTimerTraps[trap]
     local config = common.traps.timer[trap.object.id]
 
@@ -77,8 +62,9 @@ timerTrapCallback = function(trap)
     end
 end
 
+
 -- Proximity Traps
-proximityTrapCallback = function(trap)
+local function proximityTrapCallback(trap)
     local config = common.traps.proximity[trap.object.id]
 
     if (config and
@@ -106,6 +92,7 @@ end
 
 
 -- Collision Traps
+-- TODO: Unregister collision events when leaving the cell.
 local function onCollision(e)
     local trap = e.target
     if (e.mobile == tes3.mobilePlayer and
@@ -131,3 +118,16 @@ local function onCollision(e)
     end
 end
 event.register("collision", onCollision)
+
+
+local function trapTimerCallback()
+    for trapReference in pairs(activeTimerTraps) do
+        timerTrapCallback(trapReference)
+    end
+    for trapReference in pairs(activeProximityTraps) do
+        proximityTrapCallback(trapReference)
+    end
+end
+event.register("loaded", function()
+    timer.start{iterations = -1, duration = 0.15, callback=trapTimerCallback}
+end)
